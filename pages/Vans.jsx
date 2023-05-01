aa87a9336ecc8a12bf0be58e9c3976f2b373a0d7
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-05-01 19:04:06 -0400

    Link in Vans to link to individual van id

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect } from "react"
-// import { useState, useEffect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Vans () {
 
@@ -13,13 +13,15 @@ export default function Vans () {
     }, [])
 
     const vanElements = vans.map(van => (
-      <div key={van.id} className="van-tile">
-        <img src={van.imageUrl} />
-        <div className="van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}<span>/day</span></p>
-        </div>
-        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        <div key={van.id} className="van-tile">
+        <Link to={`/vans/${van.id}`} >
+          <img src={van.imageUrl} />
+          <div className="van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}<span>/day</span></p>
+          </div>
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </Link>
       </div> 
     ))
 
