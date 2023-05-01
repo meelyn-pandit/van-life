3b111e686bc0ef75053eb8c85383c01e7e4b1f45
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-05-01 16:18:48 -0400

    added additional css to vanElements

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -24,9 +24,11 @@ export default function Vans () {
     ))
 
     return (
-      <>
-        {vanElements}
-      </>
+      <div className="van-list-container">
+        <div className="van-list">
+          {vanElements}
+        </div>
+      </div>
       )
     }
   // useEffect(() => {
