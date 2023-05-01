829d75d99919c8acc113325a31472644238f4546
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-05-01 16:11:26 -0400

    added vanElements mapping feature

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -12,7 +12,17 @@ export default function Vans () {
         .then(data => setVans(data.vans))
     }, [])
 
-    console.log(vans)
+    const vanElements = vans.map(van => (
+      <div key={van.id} className="van-tile">
+        <img src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}<span>/day</span></p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </div> 
+    ))
+
     return (
       <h1>Vans page goes here</h1>
       )
