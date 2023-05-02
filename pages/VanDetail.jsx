020edfad681af35ca04c3647c24f2b19cc783973
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-05-02 17:53:37 -0400

    console logging params

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,8 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
   const params = useParams()
   console.log(params)
+
+  useEffect(() => {
+    // fetch data
+    const van_id = fetch(`/api/vans/:${params.id}`)
+    console.log('Van ID ', params.id)  
+  }, [])
   return <h1>Van detail page goes here</h1>
 }
