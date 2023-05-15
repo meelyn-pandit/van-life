e463ac8d6580f6aa83386b8211e1daaf8e0b6125
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-05-15 14:06:20 -0400

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../index.css'
+
+export default function Footer() {
+  return (
+    <footer>&#169; 2023 #VANLIFE</footer>
+  )
+}
--- index.jsx
+++ index.jsx
@@ -18,15 +18,18 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+
+          <Route path="/vans">
+            <Route index element={<Vans />} /> 
+            <Route path=":id" element={<VanDetail />} />
+          </Route>
           <Route path="/products/:productId" element={<ProductDetail />} />
-          <Route path="/host" element={<HostLayout />} >
-            <Route path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="host" element={<HostLayout />} >
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
