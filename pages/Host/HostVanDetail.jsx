63d9f6259866474149b7d912fd0e839909401310
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-06-06 17:33:44 -0400

    host van stuff

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function HostVanDetail() {
+  const params = useParams()
+  console.log('HostVanDetails params ', params)
+  const [hostVan, setHostVan] = useState(null)
+  console.log('Van ', hostVan)
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${params.hostId}`)
+      .then(res => res.json())
+      .then(data => setVan(data.vans))
+      console.log('Vans Res', params.hostId)
+  }, [params.hostId])
+
   return (
-    <h1> Host Van Detail Page</h1>
-  )
+    <div className="van-detail-container">
+      {hostVan ? (
+        <div className="van-detail">
+          <img src={hostVan.imageUrl} />
+          <i className={`van-type ${hostVan.type} selected`}>{hostVan.type}</i>
+          <h2>{hostVan.name}</h2>
+          <p className="van-price"><span>${hostVan.price}</span>/day</p>
+          <p>{hostVan.description}</p>  
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : <h2>Loading...</h2>}
+    </div>
+  ) 
 }
--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
 
-export default function HostVans() {
-  return (
-    <h1> Host Vans List Page</h1>
-  )
-}
+export default function HostVans () {
+  const[hostVans, setHostVans] = useState([])
+  useEffect(() => {
+    // fetch data
+    fetch("/api/host/vans")
+        .then(res => res.json())
+        .then(data => setHostVans(data.vans))
+    }, [])
+
+    const hostVansEls = hostVans.map(van => (
+        <div key={van.id} className="van-tile">
+        <Link to={`/host/vans/${van.id}`} >
+          <img src={van.imageUrl} />
+          <div className="van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}<span>/day</span></p>
+          </div>
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </Link>
+      </div> 
+    ))
+
+    return (
+      <section>
+        <h1 className="host-vans-title">Your listed vans</h1>
+        <div className="host-vans-list">
+          {
+          hostVans.length > 0 ? (
+            <section>
+              {hostVansEls}
+            </section>
+          ) : (
+            <h2>Loading...</h2>
+          )}
+        </div>
+      </section>
+      )
+    }
